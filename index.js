deab95008cd42143401a1b602b6b78e5777769bd
Carlyle Komalram <glaw14@gmail.com>
2022-08-22 20:53:11 -0500

    primera ruta

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express();
 
+//rutas
+app.get('/', (req, res)=> {
+    res.json({
+        ok: true,
+        msg: 'Hola Carlyle'
+    })
+});
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Servidor corriendo en puerto 3000');
