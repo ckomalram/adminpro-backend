3b714303fdfab0d650ee36db318b9d77f81e8ffd
Carlyle Komalram <glaw14@gmail.com>
2022-08-25 20:56:50 -0500

    Manejando error  duplicate key

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,27 +1,39 @@
+const { response } = require("express");
+const User = require("../models/user.model");
 
-const User = require('../models/user.model');
+const getUsers = async (req, res) => {
+  const users = await User.find({}, "name email role google");
+  res.json({
+    ok: true,
+    users,
+  });
+};
 
-const getUsers = async (req, res)=> {
-    const users = await User.find({}, 'name email role google');
+const createUser = async (req, res = response) => {
+  console.log(req.body);
+  const { email, password, name } = req.body;
+  try {
 
-
-
-    res.json({
-        ok: true,
-        users
-    })
-}
-
-const createUser = async (req, res)=> {
-    console.log(req.body);
-    const {email, password, name} = req.body;
+    const existEmail = await User.findOne({email});
+    if (existEmail) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'Correo ya registrado en otro usuario.'
+        })
+    }
     const user = new User(req.body);
-
     await user.save();
     res.json({
-        ok: true,
-        user: user
-    })
-}
+      ok: true,
+      user: user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error inesperado, revisar Logs",
+    });
+  }
+};
 
-module.exports = {getUsers,createUser}
+module.exports = { getUsers, createUser };
